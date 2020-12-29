0295cacdfd06b234b3daa8f32456d3bf45ed30c6
Sagar More <sagar.more@syspiretechnologies.com>
2020-12-29 19:42:26 +0530

    2nd Commit

--- LogUtil/src/main/java/com/sagar/logutil/MainTestActivity.kt
+++ LogUtil/src/main/java/com/sagar/logutil/MainTestActivity.kt
@@ -1,12 +1,13 @@
-package com.sagar
+package com.sagar.logutil
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainTestActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_test)
     }
 
 
